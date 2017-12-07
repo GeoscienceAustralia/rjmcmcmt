75b60b1a0590aa36fcdfd9b5e9550124a4ce707e
rcb547 <ross.c.brodie@icloud.com>
2017-12-07 13:06:02 +1100

    Changes to .gitignore and run_make.sh

--- makefiles/run_make.sh
+++ makefiles/run_make.sh
@@ -5,19 +5,19 @@ export srcdir='../src'
 export cpputilssrc='../submodules/cpp-utils/src'
 
 #GNU compiler on raijin.nci.org.au
-#module load rjmcmcmt/gnu
-#export cxx=g++
-#export mpicxx=mpiCC
-#export cxxflags='-std=c++11 -O3 -Wall -fdiagnostics-color=always'
-
+module load rjmcmcmt/gnu
+export cxx=g++
+export mpicxx=mpiCC
+export cxxflags='-std=c++11 -O3 -Wall -fdiagnostics-color=always'
+export exedir='../bin/raijin/gnu'
 
 #Intel compiler on raijin.nci.org.au
-module load librjmcmc/intel
-export cxx=icpc
-export mpicxx=mpiCC
-export cxxflags='-std=c++11 -O3 -Wall -diag-disable remark'
+# module load librjmcmc/intel
+# export cxx=icpc
+# export mpicxx=mpiCC
+# export cxxflags='-std=c++11 -O3 -Wall -diag-disable remark'
+# export exedir='../bin/raijin/intel'
 
-export exedir='../bin/raijin'
 mpiCC -showme
 make -f rjmcmcmt.make $1
 make -f generate_synthetic_data.make $1
