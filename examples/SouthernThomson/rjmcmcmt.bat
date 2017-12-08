3d90e3f0bf71cecbc8793b07c1c3bece12c974bb
rcb547 <ross.c.brodie@icloud.com>
2017-12-08 16:58:10 +1100

    Updated the examples

---
+++ examples/SouthernThomson/rjmcmcmt.bat
@@ -0,0 +1,15 @@
+@ECHO OFF
+
+REM If using MPI make sure its mpiexec is in the path
+SET path=C:\Microsoft_HPC_Pack_2012\Bin;%path%
+
+REM Make sure rjmcmcmt.exe is in the path
+SET path=..\..\bin\x64\Release;%path%
+
+REM run stand-alone
+REM rjmcmcmt.exe rjmcmcmt.con
+
+REM run with four MPI processes (and thus 32 Markov Chains)
+mpiexec -np 32 rjmcmcmt.exe rjmcmcmt.con
+
+PAUSE
